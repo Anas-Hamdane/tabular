b8b130b3db0dbb828e0a813a6dd119ec3ce61580
Anas Hamdane <anasdahi44@gmail.com>
2025-09-11 00:00:47 +0100

    Table to string

--- include/tabular/row.h
+++ include/tabular/row.h
@@ -7,12 +7,17 @@ class Row {
 public:
   class Config {
   public:
-    constexpr Config() = default;
+    explicit Config(Row& parent)
+      : parent(parent)
+    {
+    }
 
     void width(const size_t width) { this->width_ = width; }
+
     size_t width() const { return this->width_; }
 
   private:
+    Row& parent;
     size_t width_ = 50;
   };
 
@@ -25,28 +30,14 @@ public:
   }
 
   std::vector<Column> columns() const { return this->columns_; }
-  void columns(std::vector<Column> columns) const
-  {
-    this->columns_ = std::move(columns);
-  }
+  void columns(std::vector<Column> columns) { this->columns_ = std::move(columns); }
 
   Config& config() { return this->config_; }
   const Config& config() const { return this->config_; }
 
   std::string toStr() const
   {
-    if (columns_.empty()) return "";
-
-    const size_t estimatedWidth = config().width();
-
-    size_t unspecified = 0;
-    size_t rowWidth = calculateWidth(unspecified);
-
-    if (rowWidth < estimatedWidth && unspecified != 0)
-      adjustUnspecifiedWidth(unspecified);
-    else
-      adjustWidth();
-
+    if (this->columns_.empty()) return "";
     std::string row;
 
     size_t i = 0;
@@ -70,9 +61,9 @@ public:
       }
 
       row += '|';
-      row += '\n';
-
       if (!hasColumn) break;
+
+      row += '\n';
       ++i;
     }
 
@@ -80,67 +71,9 @@ public:
   }
 
 private:
-  // make sure it is not changeable EXCEPT in `adjustWidth()`
-  // since before printing we have to make sure all the columns
-  // have the correct width set
-  mutable std::vector<Column> columns_ = {};
-  Config config_;
+  std::vector<Column> columns_ = {};
+  Config config_ = Config(*this);
 
-  size_t calculateWidth(size_t& unspecified) const
-  {
-    if (columns_.empty()) return 0;
-    size_t width = this->columns_.size() + 1;
-
-    for (const auto& column : this->columns_)
-    {
-      size_t w = column.config().width();
-
-      if (w == 0) unspecified++;
-      else width += w;
-    }
-
-    return width;
-  }
-  void adjustWidth() const
-  {
-    size_t width = config().width();
-
-    // subtract the splits
-    width -= (this->columns_.size() + 1);
-    size_t indivWidth = width / this->columns_.size();
-    size_t rest = width % this->columns_.size();
-
-    for (auto& column : this->columns_)
-    {
-      column.config().width(indivWidth + (rest > 0));
-      rest -= (rest > 0);
-    }
-
-    // if there's still a rest
-    if (rest > 0) this->columns_.back().config().width(indivWidth + rest);
-  }
-  void adjustUnspecifiedWidth(size_t unspecified) const
-  {
-    size_t width = config().width();
-
-    // subtract the splits
-    width -= (this->columns_.size() + 1);
-    size_t indivWidth = width / unspecified;
-    size_t rest = width % unspecified;
-
-    size_t lastUnsp = 0;
-    for (size_t i = 0; i < this->columns_.size(); ++i)
-    {
-      auto& column = this->columns_[i];
-      if (column.config().width() != 0) continue;
-
-      column.config().width(indivWidth + (rest > 0));
-      rest -= (rest > 0);
-      lastUnsp = i;
-    }
-
-    // if there's still a rest
-    if (rest > 0) this->columns_[lastUnsp].config().width(indivWidth + rest);
-  }
+  friend class Table;
 };
 }
--- include/tabular/table.h
+++ include/tabular/table.h
@@ -4,6 +4,26 @@
 #include "border.h"
 
 namespace tabular {
+
+namespace detail {
+inline bool bisearch(const std::vector<size_t>& vec, size_t value)
+{
+  size_t left = 0;
+  size_t right = vec.size();
+
+  while (left < right)
+  {
+    size_t mid = left + (right - left) / 2;
+
+    if (vec[mid] == value) return true;
+
+    if (vec[mid] < value) left = mid + 1;
+    else right = mid;
+  }
+  return false;
+}
+}
+
 class Table {
 public:
   class Config {
@@ -19,8 +39,11 @@ public:
 
 public:
   constexpr Table() = default;
+
   Table(std::vector<Row> rows, Border border = {})
-    : rows_(std::move(rows)), border(std::move(border)) {}
+    : rows_(std::move(rows)), border_(border)
+  {
+  }
 
   std::vector<Row> rows();
   void rows(std::vector<Row> rows);
@@ -28,26 +51,203 @@ public:
   Config& config() { return this->config_; }
   const Config& config() const { return this->config_; }
 
+  Border& border() { return this->border_; }
+  const Border& border() const { return this->border_; }
+
   std::string toStr() const
   {
+    if (this->rows_.empty()) return "";
+
+    std::string table;
     adjustWidth();
-    // and put them in a std::string
+
+    table += getBorderHeader() + '\n';
+
+    const size_t rowsSize = this->rows_.size();
+    for (size_t i = 0; i < rowsSize; ++i)
+    {
+      const auto& row = this->rows_[i];
+
+      table += row.toStr() + '\n';
+      if (i + 1 < rowsSize) table += getBorderMiddle(i) + '\n';
+    }
+
+    table += getBorderFooter();
+    return table;
   }
 
 private:
   mutable std::vector<Row> rows_ = {};
   Config config_ = {};
-  Border border = {};
+  Border border_ = {};
+
+  size_t calculateWidth(const Row& row, size_t& unspecified) const
+  {
+    const auto& columns = row.columns();
+    if (columns.empty()) return 0;
+    size_t width = columns.size() + 1;
+
+    for (const auto& column : columns)
+    {
+      size_t w = column.config().width();
+
+      if (w == 0) unspecified++;
+      else width += w;
+    }
+
+    return width;
+  }
+  void setWidth(Row& row) const
+  {
+    auto& columns = row.columns_;
+    size_t width = config().width();
+
+    // subtract the splits
+    width -= (columns.size() + 1);
+    size_t indivWidth = width / columns.size();
+    size_t rest = width % columns.size();
+
+    for (auto& column : columns)
+    {
+      column.config().width(indivWidth + (rest > 0));
+      rest -= (rest > 0);
+    }
+
+    // if there's still a rest
+    if (rest > 0) columns.back().config().width(indivWidth + rest);
+  }
+  void setUnspecifiedWidth(Row& row, size_t unspecified) const
+  {
+    auto& columns = row.columns_;
+    size_t width = config().width();
+
+    // subtract the splits
+    width -= (columns.size() + 1);
+    size_t indivWidth = width / unspecified;
+    size_t rest = width % unspecified;
+
+    size_t lastUnsp = 0;
+    for (size_t i = 0; i < columns.size(); ++i)
+    {
+      auto& column = columns[i];
+      if (column.config().width() != 0) continue;
+
+      column.config().width(indivWidth + (rest > 0));
+      rest -= (rest > 0);
+      lastUnsp = i;
+    }
+
+    // if there's still a rest
+    if (rest > 0) columns[lastUnsp].config().width(indivWidth + rest);
+  }
 
   void adjustWidth() const
   {
     if (this->rows_.empty()) return;
     const size_t width = config().width();
 
-    for (auto& row : this->rows_)
+    for (Row& row : this->rows_)
     {
-      row.config().width(width);
+      const size_t estimatedWidth = config().width();
+
+      size_t unspecified = 0;
+      const size_t rowWidth = calculateWidth(row, unspecified);
+
+      if (rowWidth < estimatedWidth && unspecified != 0)
+        setUnspecifiedWidth(row, unspecified);
+      else
+        setWidth(row);
     }
+  }
+
+  std::vector<size_t> connections(const Row& row) const
+  {
+    std::vector<size_t> connections;
+    connections.reserve(row.columns().size());
+
+    size_t track = 1;
+    for (const Column& column : row.columns())
+    {
+      track += column.config().width();
+      connections.push_back(track++);
+    }
+
+    return connections;
+  }
+  std::string getBorderHeader() const
+  {
+    const auto& columns = this->rows_[0].columns();
+
+    std::string header = this->border_.cornerTopLeft();
+    header.reserve(config().width());
+
+    for (size_t i = 0; i < columns.size(); ++i)
+    {
+      size_t width = columns[i].config().width();
+
+      for (size_t j = 0; j < width; ++j)
+          header += this->border_.horizontal();
+
+      if (i + 1 < columns.size()) header += this->border_.connectorTop();
+    }
+
+    header += this->border_.cornerTopRight();
+    return header;
+  }
+  std::string getBorderFooter() const
+  {
+    const auto& columns = this->rows_.back().columns();
+
+    std::string footer = this->border_.cornerBottomLeft();
+    footer.reserve(config().width());
+
+    for (size_t i = 0; i < columns.size(); ++i)
+    {
+      size_t width = columns[i].config().width();
+
+      for (size_t j = 0; j < width; ++j)
+        footer += this->border_.horizontal();
+
+      if (i + 1 < columns.size()) footer += this->border_.connectorBottom();
+    }
+
+    footer += this->border_.cornerBottomRight();
+    return footer;
+  }
+  std::string getBorderMiddle(size_t index) const
+  {
+    const auto nextRowConnections = connections(this->rows_[index + 1]);
+    const auto& border = this->border_; // I like this alias
+
+    std::string middle = border.connectorLeft();
+    middle.reserve(config().width());
+
+    const auto& columns = this->rows_[index].columns();
+
+    size_t tracker = 0;
+    for (size_t i = 0; i < columns.size(); ++i)
+    {
+      size_t width = columns[i].config().width();
+
+      for (size_t j = 0; j < width; ++j)
+      {
+        if (detail::bisearch(nextRowConnections, ++tracker))
+          middle += border.connectorTop();
+        else
+          middle += border.horizontal();
+      }
+      tracker++;
+
+      if (i + 1 >= columns.size()) continue;
+
+      if (detail::bisearch(nextRowConnections, tracker))
+        middle += border.intersection();
+      else
+        middle += border.connectorBottom();
+    }
+
+    middle += border.connectorRight();
+    return middle;
   }
 };
 }
